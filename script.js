08ea65f5e6fe59bff5e2d6b1e36b25bb7e4e5934
KAISAR LONE <90574862+kaisu0255@users.noreply.github.com>
2022-05-25 11:10:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//============ typing animation  =============///
+
+var typed = new Typed(".typing",{
+    strings:["","Web Design", "Web Developer","Graphic Designer","YouTuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
